c46120819a7481e1574e9c3b718020ad7357cd9f
eddie <edgardojosemedina2@gmail.com>
2019-11-08 19:40:27 -0500

    swiper.css readded

---
+++ js/popup.js
@@ -0,0 +1,7 @@
+$('.js-modal-show').click(function(){
+    $('.js-modal-shopify').toggleClass('is-shown--off-flow').toggleClass('is-hidden--off-flow');
+  });
+  
+  $('.js-modal-hide').click(function(){
+    $('.js-modal-shopify').toggleClass('is-shown--off-flow').toggleClass('is-hidden--off-flow');
+  });
