54bad890b958ec075a0da372f8fa67732314e448
eddie <edgardojosemedina2@gmail.com>
2019-11-08 20:32:16 -0500

    site roll back

--- js/popup.js
+++
@@ -1,7 +0,0 @@
-$('.js-modal-show').click(function(){
-    $('.js-modal-shopify').toggleClass('is-shown--off-flow').toggleClass('is-hidden--off-flow');
-  });
-  
-  $('.js-modal-hide').click(function(){
-    $('.js-modal-shopify').toggleClass('is-shown--off-flow').toggleClass('is-hidden--off-flow');
-  });
